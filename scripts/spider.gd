42dccffcc7646658fcfe3c71bffbe8c58f22a3f4
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2024-10-05 12:18:41 +0100

    Spider code changes

--- scripts/spider.gd
+++ scripts/spider.gd
@@ -5,7 +5,6 @@ const SPEED = 20.0
 const JUMP_VELOCITY = 4.5
 var control_camera = false
 
-
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -31,6 +30,13 @@ func _physics_process(delta: float) -> void:
 func _process(delta: float) -> void:
 	if control_camera:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		var basis = $Camera.get_global_transform().basis.z
+		var rot_x = basis.x * -0.003
+		var rot_z = basis.z * -0.003
+		$".".position.x += rot_x  
+		$".".position.z += rot_z
+		#position_object_local(Vector3(1, 0, 0), rot_x) # first rotate in X
+		#position_object_local(Vector3(0, 0, 1), rot_z) # then rotate in Z
 	else:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		
@@ -38,7 +44,9 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.is_action_pressed("control_camera"):
 			control_camera = true
-			print("X: %s Y: %s" % [fmod($Camera.rotation_degrees.x, 360.0), fmod($Camera.rotation_degrees.y, 360.0)])
+			#print("X: %s Z: %s" % [$".".position.x, $".".position.z])
+			print($Camera.get_global_transform().basis.z)
+			#print("X: %s Y: %s" % [fmod($Camera.rotation_degrees.x, 360.0), fmod($Camera.rotation_degrees.y, 360.0)])
 		if event.is_action_released("control_camera"):
 			control_camera = false
 	if event is InputEventMouseMotion and control_camera:
